7c7f4fde27cedd4a9699780ecddbabbed9ae9f3a
Natchaipon <peepee.popoz@gmail.com>
2019-09-30 01:52:33 +0700

    Update Project.java

--- Project.java
+++ Project.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.time.LocalDateTime;  
 import java.time.format.DateTimeFormatter;  
 
-public class Project {
+class Project {
   int year;
   String formattedDate;
 
@@ -22,15 +22,36 @@ public class Project {
   public static void main(String[] args) {
     Project get_year_now = new Project();
     Project get_year_birth = new Project();
+    Check_Age check_age_total = new Check_Age();
+
     while (true) {
         get_year_now.input_year();
         get_year_birth.year_now_get();
-        // System.out.println(myObj.year);
         int year_now = Integer.parseInt(get_year_birth.formattedDate);
-        // System.out.println(myObj1.formattedDate);
         int age = year_now - get_year_now.year;
         System.out.print("Age Now : ");
         System.out.println(age);
+        check_age_total.check_age(age);
+    }
+  }
+}
+
+class Check_Age {
+  public static void check_age(int x){
+    if(x <= 12){
+      System.out.println("วัยเด็ก");
+    }
+    else if(x <= 25){
+      System.out.println("วัยรุ่น");
+    }
+    else if(x <= 40){
+      System.out.println("วัยกลางคน");
+    }
+    else if(x <= 60){
+      System.out.println("วัยทอง");
+    }
+    else if(x > 60){
+      System.out.println("ผู้สูงอายุ");
     }
   }
 }
